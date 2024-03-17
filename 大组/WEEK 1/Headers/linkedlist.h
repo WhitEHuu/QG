2c05cf72e3bc8a4f1fc82860f31e21cdb85901e5
WhitE <153378857+WhitEHuu@users.noreply.github.com>
2024-03-17 14:14:14 +0800

    Update linkedlist.h

--- 大组/WEEK 1/Headers/linkedlist.h
+++ 大组/WEEK 1/Headers/linkedlist.h
@@ -4,16 +4,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//定义单向链表节点结构体
 typedef struct ListNode {
     int data;
     struct ListNode *next;
 } ListNode;
 
+//创建新的单向链表节点
 ListNode* createNode(int data);
+
+//在单向链表头插入新节点
 void insertAtBeginning(ListNode** head, int data);
+
+//在单向链表尾插入新节点
 void insertAtEnd(ListNode** head, int data);
+
+//删除单向链表中的一个指定节点
 void deleteNode(ListNode** head, int key);
+
+//打印单向链表
 void printList(ListNode* head);
+
+//释放单向链表内存
 void freeList(ListNode** head);
 
 #endif
