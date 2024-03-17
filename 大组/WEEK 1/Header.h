b8a63dfb67cd7a5da9798c691226ddd590976f74
WhitE <153378857+WhitEHuu@users.noreply.github.com>
2024-03-17 12:34:30 +0800

    Update Header.h

--- 大组/WEEK 1/Header.h
+++ 大组/WEEK 1/Header.h
@@ -1,1 +1,19 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct ListNode {
+    int data;
+    struct ListNode *next;
+} ListNode;
+
+ListNode* createNode(int data);
+void insertAtBeginning(ListNode** head, int data);
+void insertAtEnd(ListNode** head, int data);
+void deleteNode(ListNode** head, int key);
+void printList(ListNode* head);
+void freeList(ListNode** head);
+
+#endif
